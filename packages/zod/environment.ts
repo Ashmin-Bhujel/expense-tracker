6f9334314153fd8d76c1b53242e180a72c7fddd1
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-03-27 11:11:39 +0545

    feat(zod): add mongodb uri and dbname to api schema

--- packages/zod/environment.ts
+++ packages/zod/environment.ts
@@ -2,14 +2,19 @@ import * as z from "zod";
 
 // * Schemas
 export const apiEnvironmentZodSchema = z.object({
+  // * Node
   NODE_ENV: z
-    .enum(["development", "production"], "Please set the valid node environment")
+    .enum(["development", "production"], "Please set the valid NODE_ENV")
     .default("production"),
+  // * Server
   PORT: z.coerce
-    .number("Please provide a server port number")
-    .min(5000, "The server port number should be greater than 5000")
-    .max(6000, "The server port number should be less than 6000")
+    .number("Please set the server PORT")
+    .min(5000, "The server PORT number should be greater than 5000")
+    .max(6000, "The server PORT number should be less than 6000")
     .default(5000),
+  // * MongoDB
+  MONGODB_URI: z.string("Please set the MONGODB_URI"),
+  MONGODB_DBNAME: z.string("Please set the MONGODB_DBNAME"),
 });
 
 // * Types
