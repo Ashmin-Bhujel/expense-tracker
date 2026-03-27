5f8fbabb0b59c60b136359d5b87919a239c55ecc
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-03-27 10:39:18 +0545

    refactor(api): use error handler middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import environment from "./environment.js";
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
 import { logger } from "./middlewares/logger.middleware.js";
 import { notFound } from "./middlewares/not-found.middleware.js";
+import { ApiResponse } from "./utils/api-response.js";
 
 // * Express config
 export const app = express();
@@ -20,10 +22,11 @@ if (environment.NODE_ENV === "development") {
 
 // * Routes
 app.get("/", (_req, res) => {
-  return res.status(200).json({
-    message: "Server is up and running",
-  });
+  return res.json(new ApiResponse("Server is up and running"));
 });
 
 // * Not found route handler
 app.use(notFound);
+
+// * Error handler
+app.use(errorHandler);
