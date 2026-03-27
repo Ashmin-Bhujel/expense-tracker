3f03aa856e09b355a2c745121756734c1c9e742a
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-03-27 11:12:51 +0545

    chore(api): database connection

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,20 @@
 import { app } from "./app.js";
 import environment from "./environment.js";
+import { connectDB } from "./services/db.service.js";
 
-// * Server
-app.listen(environment.PORT, () => {
-  console.log(`Server is up and running or port ${environment.PORT}`);
-});
+// * Connect to database
+connectDB()
+  .then(() => {
+    console.log("Connected to database successfully");
+
+    // * Server
+    app.listen(environment.PORT, () => {
+      console.log(`Server is up and running on port ${environment.PORT}`);
+    });
+  })
+  .catch((error) => {
+    if (error instanceof Error) {
+      console.log(error.message);
+      process.exit(1);
+    }
+  });
