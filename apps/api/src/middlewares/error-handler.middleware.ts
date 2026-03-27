d05b5c152fdbf7a5033130ed0cb091d15b8c7757
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-03-27 14:58:21 +0545

    refactore(api): add support for errors from mongoose and zod in error handler

--- apps/api/src/middlewares/error-handler.middleware.ts
+++ apps/api/src/middlewares/error-handler.middleware.ts
@@ -1,15 +1,29 @@
 import type { NextFunction, Request, Response } from "express";
 
 import { ApiError } from "@/utils/api-error.js";
+import { MongooseError } from "mongoose";
+import { ZodError } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorHandler(error: unknown, _req: Request, res: Response, _next: NextFunction) {
   const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
 
+  // * Handle mongoose errors
+  if (error instanceof MongooseError) {
+    return res.json(new ApiError(`Mongoose Error: ${error.name}`, [error.message]));
+  }
+
+  // * Handle zod errors
+  if (error instanceof ZodError) {
+    return res.json(new ApiError(`Validation Error: ${error.name}`, null, error.issues));
+  }
+
+  // * Handle thrown errors
   if (error instanceof Error) {
     return res.json(new ApiError(error.message));
   }
 
+  // * Handle any other errors
   return res.json(new ApiError("Something went wrong"));
 }
