370943d4991522a87afc5f5020276e0e6d18036a
Ashmin Bhujel <ashminbhujel01@gmail.com>
2026-03-28 15:13:30 +0545

    feat(zod): add schema and type for login user and api user response

--- packages/zod/user.ts
+++ packages/zod/user.ts
@@ -16,8 +16,26 @@ export const createUserZodSchema = userZodSchema.omit({
   id: true,
 });
 export const updateUserZodSchema = createUserZodSchema.partial();
+export const loginUserZodSchema = userZodSchema.pick({ email: true, password: true });
+export const loginUserResponseZodSchema = userZodSchema
+  .extend({
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+    refreshToken: z.string(),
+    accessToken: z.string(),
+  })
+  .omit({ password: true });
+export const apiUserResponseZodSchema = userZodSchema
+  .extend({
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+  })
+  .omit({ password: true });
 
 // * Types
 export type UserType = z.infer<typeof userZodSchema>;
 export type CreateUserType = z.infer<typeof createUserZodSchema>;
 export type UpdateUserType = z.infer<typeof updateUserZodSchema>;
+export type LoginUserType = z.infer<typeof loginUserZodSchema>;
+export type LoginUserResponseType = z.infer<typeof loginUserResponseZodSchema>;
+export type ApiUserResponseType = z.infer<typeof apiUserResponseZodSchema>;
